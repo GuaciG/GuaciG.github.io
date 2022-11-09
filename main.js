98e93cee5d96ca59e6bfa5c96bd2b84880c0795f
Guaci Gutierrez <guacig@hotmail.com>
2022-11-09 19:32:53 +0000

    created template literals in js file for projects

--- main.js
+++ main.js
@@ -1,4 +1,198 @@
-/* add an event listener to the document, when the page finishs to load,
+/* data for projects*/
+const projects = [
+	{
+		projectType: "react",
+		href: "https://github.com/GuaciG/netflix-clone",
+		title: "Netflix Clone",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651502/Proyectos/Netflix%20clone/netflix-preview_xnstdg.jpg",
+		details: "React / Redux / TMDB Api / Firebase"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/rock-paper-scissors-game",
+		title: "Rock-Paper-Scissors Game",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651660/Proyectos/Rock-Paper-Scissors/rock-paper-scissors-game-app-preview_tcfgll.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/techiesgirls-website",
+		title: "TechiesGIRLS Website",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651580/Proyectos/TechiesGIRLS-website/techies-girls-preview_hy5rqn.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "react",
+		href: "https://github.com/GuaciG/todo-list-react-app",
+		title: "ToDo List React App",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667922766/Proyectos/To-Do%20List/todo-list-app-preview_ti0bg6.jpg",
+		details: "React Hooks"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/tic-tac-toe-game",
+		title: "Tic Tac Toe Game",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651628/Proyectos/TicTacToe/tic-tac-toe-game-app-preview_e3k157.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/local-weather-app",
+		title: "Local Weather App",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667922826/Proyectos/Weather/local-weather-app-preview_hpcglg.jpg",
+		details: "JavaScript / Openweather API"
+	},
+	{
+		projectType: "react",
+		href: "https://github.com/GuaciG/pomodoro-timer",
+		title: "Pomodoro Clock",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667922881/Proyectos/Pomodoro%20Clock/pomodoro-clock-app-preview_dvikgy.jpg",
+		details: "React"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/calculator",
+		title: "JavaScript Calculator",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651312/Proyectos/Calculator/calculator-app-preview_zertq3.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/drum-machine",
+		title: "Drum Machine",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667922649/Proyectos/Drum%20Machine/drum-machine-app-preview_y5dvml.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "javaScript",
+		href: "https://github.com/GuaciG/landing-page-travel",
+		title: "Travel Single Page",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651532/Proyectos/Product%20Landing%20Page/travel-single-page-preview_wqhyrj.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "react",
+		href: "https://github.com/GuaciG/random-quote-machine",
+		title: "Random Quote Machine",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667922941/Proyectos/Random%20Quote%20Machine/random-quote-machine-app-preview_sbuilt.jpg",
+		details: "React"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/product-landing-page",
+		title: "Product Landing Page",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651558/Proyectos/Product%20Landing%20Page%20Cookies/cookies-preview_efqcko.jpg",
+		details: "JavaScript"
+	},
+	{
+		projectType: "react",
+		href: "https://github.com/GuaciG/markdown-previewer",
+		title: "Markdown Previewer",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667651474/Proyectos/Markdown%20Previewer/markdown-previewer-preview_zx6mqs.jpg",
+		details: "React"
+	},
+	{
+		projectType: "javascript",
+		href: "https://github.com/GuaciG/tribute-page",
+		title: "Tribute Page",
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1667821798/Proyectos/Cate%20Blanchett/tribute-page-preview_zyw4h3.jpg",
+		details: "JavaScript"
+	}
+];
+
+/* data for languages or tools logos */
+const toolsLogos = [
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885808/iconos/html5_l9uaty.png",
+		alt: "html"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885808/iconos/css_sa8yg3.png",
+		alt: "css"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1618478985/iconos/JavaScript_ehfdft.jpg",
+		alt: "javascript"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885809/iconos/sass_ytfiqd.png",
+		alt: "sass"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1613751254/iconos/jquery_qflxqt.png",
+		alt: "jquery"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885809/iconos/react_uras0m.png",
+		alt: "react"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885808/iconos/bootstrap_smrtex.png",
+		alt: "bootstrap"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885808/iconos/git_dvm9dx.png",
+		alt: "git"
+	},
+	{
+		src: "https://res.cloudinary.com/drpcjt13x/image/upload/v1600885808/iconos/github_gf0t2u.png",
+		alt: "github"
+	}
+];
+
+/* JavaScript Template Literals to create projects and tools-logos sections */
+/* Template Literals for projects section */
+const projectTemplate = project => {
+	return `
+				<div class="project-tile ${project.projectType}">
+				<a
+					href=${project.href}
+					title=${project.title}
+					target="_blank"
+					><img
+						src=${project.src}
+						alt=${project.title}
+					/>
+					<div class="overlay">
+						<p>
+							<strong>Netflix Clone</strong>
+							<br />
+							<span class="details">
+								${project.details}
+							</span>
+						</p>
+					</div>
+				</a>
+			</div>
+		`;
+};
+
+document.getElementById("display-proyects").innerHTML = `
+	<div class="projects-box-wrapper">
+		<div class="projects-box">
+			${projects.map(projectTemplate).join("")}
+		</div>
+	</div>
+
+`;
+
+/* Template Literals for tools-logos section */
+const toolsTemplate = tool => {
+	return `
+		<img
+			src=${tool.src}
+			alt=${tool.alt}
+		/>
+	`;
+};
+
+document.getElementById("display-tools").innerHTML = `
+	<div class="combo">
+		${toolsLogos.map(toolsTemplate).join("")}
+	</div>
+`;
+
+/* add an event listener to the document, when the page finishes loading,
 runs the functions: filterProjects and typeWriter */
 document.addEventListener(
 	"DOMContentLoaded",
@@ -9,6 +203,7 @@ document.addEventListener(
 	false
 );
 
+/* filterProjects function to display projects based on the button clicked. */
 function filterProjects(selection) {
 	let i;
 	const projects = document.getElementsByClassName("project-tile");
@@ -50,7 +245,7 @@ function removeClass(element, name) {
 	element.className = arr1.join(" ");
 }
 
-// Add active class to the current control button (highlight it)
+// Add 'highlighted' class to the current control button (highlight it)
 const btnContainer = document.getElementById("btns-wrapper");
 const btns = btnContainer.getElementsByClassName("btn");
 
@@ -62,7 +257,22 @@ for (let i = 0; i < btns.length; i++) {
 	});
 }
 
-/* navigation to specific section via menu-links  */
+/* Animation typeWriter in title  */
+const H3 = document.getElementById("role");
+const txt = "Front-End Developer.";
+const speed = 200;
+let letter = 0;
+
+function typeWriter() {
+	//if letter < txt.length writes in h3 the character in that position
+	if (letter < txt.length) {
+		H3.innerHTML += txt.charAt(letter);
+		letter++;
+		setTimeout(typeWriter, speed);
+	}
+}
+
+/* navigation-scroll to specific section via navbar links */
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".nav-content .nav-links li a");
 
@@ -84,19 +294,3 @@ window.addEventListener("scroll", () => {
 		}
 	});
 });
-
-/* Animation typeWriter in title  */
-
-const H3 = document.getElementById("role");
-const txt = "Front-End Developer.";
-const speed = 200;
-let letter = 0;
-
-function typeWriter() {
-	//if letter < txt.length writes in h3 the character in that position
-	if (letter < txt.length) {
-		H3.innerHTML += txt.charAt(letter);
-		letter++;
-		setTimeout(typeWriter, speed);
-	}
-}
